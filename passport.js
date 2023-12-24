0fc5e86e1c28713c0abbf2fe7c86d07c215f57c8
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-12-24 09:42:37 +0100

    feat: add production app

--- passport.js
+++ passport.js
@@ -1,10 +1,20 @@
-const DOMAIN = "http://localhost:5500/"
+let DOMAIN
+let CLIENT_ID
+
+if(window.location.href.includes("localhost")){
+  DOMAIN = "http://localhost:5500/"
+  CLIENT_ID = "LOCmHiZuAGofHBVlOkAcHbJh80Z8DYhJ"
+  }else{
+  DOMAIN = "https://stackup-invaders.vercel.app/"
+  CLIENT_ID = "1jOXOSHt33RhUrLrl0R0tgv3Kn4J2WPY";
+}
+
 
 window.passport = new window.immutable.passport.Passport({
   baseConfig: new window.immutable.config.ImmutableConfiguration({
     environment: window.immutable.config.Environment.SANDBOX,
   }),
-  clientId: "LOCmHiZuAGofHBVlOkAcHbJh80Z8DYhJ",
+  clientId: CLIENT_ID,
   redirectUri: DOMAIN,
   logoutRedirectUri: DOMAIN + "logout.html",
   audience: "platform_api",
