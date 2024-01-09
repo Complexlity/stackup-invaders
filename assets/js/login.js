53b68a149316315c68a4030661510cd7ab527bcd
Complexlity <105590967+Complexlity@users.noreply.github.com>
2024-01-09 08:14:40 +0100

    feat: update overall highscore on page

--- assets/js/login.js
+++ assets/js/login.js
@@ -25,7 +25,7 @@ const client = new window.immutable.blockchainData.BlockchainData(config);
 
 const getUserInfo = async function () {
   const highestScoringApi = "https://immutable-game.vercel.app/api/scoring";
-  const [userProfile, highestScore] = await Promise.all([window.passport.getUserInfo(), fetch(highestScoringApi).then(res => res.json())])
+  const [userProfile, {highestScore}] = await Promise.all([window.passport.getUserInfo(), fetch(highestScoringApi).then(res => res.json())])
   window.userProfile = {...userProfile, address:window.accounts[0]}
   window.highestScore = highestScore
 
