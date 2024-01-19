183e6b8c6bcc23bed490af85174bb766c2fe5be6
Complexlity <105590967+Complexlity@users.noreply.github.com>
2024-01-19 15:55:44 +0100

    chore: deploy new passport client

--- assets/js/passport.js
+++ assets/js/passport.js
@@ -5,16 +5,16 @@ let url = window.location.href;
 
 if(url.includes("localhost")){
   DOMAIN = "http://localhost:5500/"
-  CLIENT_ID = "LOCmHiZuAGofHBVlOkAcHbJh80Z8DYhJ"
+  CLIENT_ID = "7pcx6llzmGnX9WZBRmA683HpgSDbYWkV";
 }
 else if (url.includes('vercel')) {
   DOMAIN = "https://stackup-invaders.vercel.app/"
-  CLIENT_ID = "1jOXOSHt33RhUrLrl0R0tgv3Kn4J2WPY";
+  CLIENT_ID = "rr2dn32f3eDZQIsBD0aF6YtGn0sWLRCq";
 
 }
 else {
   DOMAIN = "https://stackup-invaders.netlify.app/";
-  CLIENT_ID = "zmXQZuc3dBGQMTF9e1QaFJxRbpTUxiM3";
+  CLIENT_ID = "IQH9SkHcHTWQDSyUHUo3QW7YbZR9LHoM";
 }
 
 
